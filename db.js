0fe34f45ca93ae62ca115b29722518c0b9094e51
YanJi13 <jeanebernardbermudez@gmail.com>
2025-03-14 01:46:24 +0800

    Added port to .env and db.js

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const db = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  port: process.env.PORT
 }).promise();
 
 // Set up user info
